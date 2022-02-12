f5f31a07a08d7f51ccb449cb8b94aee43bf11e54
joel2145 <kikkun0513@gmail.com>
2022-02-12 16:58:01 +0900

    ログイン機能を実装

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
 import { ListPage } from "./pages/ListPage";
 import { MapPage } from "./pages/MapPage";
 import { SignUpPage } from "./pages/SignUpPage";
+import { LoginPage } from "./pages/LoginPage";
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<SignUpPage />}></Route>
-        <Route exact path="/list" element={<ListPage />}></Route>
-        <Route exact path="/map" element={<MapPage />}></Route>
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route exact path="/sign_up" element={<SignUpPage />}></Route>
+          <Route exact path="/login" element={<LoginPage />}></Route>
+          <Route exact path="/list" element={<ListPage />}></Route>
+          <Route exact path="/map" element={<MapPage />}></Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import { auth } from '../firebase';
+import { useNavigate, Navigate } from "react-router-dom";
+import { useAuthContext } from '../context/AuthContext';
 
 export const ListPage = () => {
-  return (
-    <div>ListPage</div>
-  )
-}
+  const navigate = useNavigate();
+  const { user } = useAuthContext();
+
+  const handleLogout = () => {
+    auth.signOut();
+    navigate('/login');
+  };
+  if (!user) {
+    return <Navigate to="/login" />;
+  } else {
+    return (
+      <div>
+        <h1>リストページ</h1>
+        <button onClick={handleLogout}>ログアウト</button>
+      </div>
+    );
+  }
+};
+
+
